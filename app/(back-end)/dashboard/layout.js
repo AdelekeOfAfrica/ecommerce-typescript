9dd1fa3c25daeea739c12e0d30c3fd50757d2a17
AdelekeOfAfrica <adelekeofafrica@gmail.com>
2025-02-23 16:47:26 +0100

    worked on designing the background

--- app/(back-end)/dashboard/layout.js
+++ app/(back-end)/dashboard/layout.js
@@ -3,14 +3,14 @@ import Sidebar from '@/components/backoffice/Sidebar.jsx'
 import Navbar from '@/components/backoffice/Navbar.jsx'
 export default function Layout({children}) {
   return (
-    <div className='flex'>
+    <div className='flex '>
       {/*side bar*/}
         <Sidebar />
 
         <div className="w-full">
           {/*header*/}
           <Navbar/>
-          <main>
+          <main className="ml-60 p-8 bg-slate-900 text-slate-50 min-h-screen mt-16">
             {children}
           </main>
           {/*main*/}
