a7faba9ba71c639f6300b8b6f9a06dfb32262c90
AdelekeOfAfrica <adelekeofafrica@gmail.com>
2025-02-27 14:57:01 +0100

    installed faker and added the line chart to the weeklysaleschart

--- components/backoffice/WeeklySalesChart.jsx
+++ components/backoffice/WeeklySalesChart.jsx
@@ -1,18 +1,93 @@
 "use client";
 
 import React, { useState } from "react";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,} from 'chart.js';
+
+import { Line } from 'react-chartjs-2';
+import {faker} from '@faker-js/faker';
+
+export const options = {
+  responsive: true,
+  plugins: {
+    legend: {
+      position: 'top',
+    },
+    title: {
+      display: false,
+      text: 'Chart.js Line Chart',
+    },
+  },
+};
+
+
+const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+export const data = {
+  labels,
+  datasets: [
+    {
+      label: 'Sales',
+      data: labels.map(() => faker.number.int({ min: -1000, max: 1000 })),
+      borderColor: 'rgb(255, 99, 132)',
+      backgroundColor: 'rgba(255, 99, 132, 0.5)',
+    },
+
+  ],
+};
+
+
 
 export default function WeeklySalesChart() {
-  const tabs = [
-    {
-      title: "Sales",
-      type: "sales",
+
+  ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+const tabs = [
+  {
+    title: "Sales",
+    type: "sales",
+    data: {
+      labels,
+      datasets: [
+        {
+          label: "Sales",
+          data: labels.map(() => faker.number.int({ min: -1000, max: 1000 })),
+          borderColor: "rgb(255, 99, 132)",
+          backgroundColor: "rgba(255, 99, 132, 0.5)",
+        },
+      ],
     },
-    {
-      title: "Orders",
-      type: "orders",
+  },
+  {
+    title: "Orders",
+    type: "orders",
+    data: {
+      labels,
+      datasets: [
+        {
+          label: "Orders",
+          data: labels.map(() => faker.number.int({ min: -1000, max: 1000 })),
+          borderColor: "rgb(53, 162, 235)",
+          backgroundColor: "rgba(53, 162, 235, 0.5)",
+        },
+      ],
     },
-  ];
+  },
+];
 
   const [chartToDisplay, setChartToDisplay] = useState(tabs[0].type);
 
@@ -44,8 +119,7 @@ export default function WeeklySalesChart() {
         {
           tabs.map((tab,i)=>{
             if(chartToDisplay===tab.type){
-              return <h3 key={i}>{tab.title}</h3>
-               
+               return <Line key={i} options={options} data={tab.data} />;
               
             }
             return null;
