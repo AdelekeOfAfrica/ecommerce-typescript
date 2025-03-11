bcfe50c33aad6e4d096a37c1a7c6cc343c19223d
AdelekeOfAfrica <adelekeofafrica@gmail.com>
2025-03-11 17:00:31 +0100

    working on the sidebar

--- components/backoffice/Sidebar.jsx
+++ components/backoffice/Sidebar.jsx
@@ -1,25 +1,105 @@
+"use client"
+
 import React from 'react';
 import Link from 'next/link';
+import Image from 'next/image';
+import logo from '../../public/logo.png';
+import { ExternalLink, LayoutGrid, Slack, Tractor, Truck, Users2, UserSquare2, Warehouse } from 'lucide-react';
+import { usePathname } from 'next/navigation';
 
 
 export default function Sidebar() {
-  return (
-    <div className="dark:bg-slate-700 bg-slate-50 space-y-6 w-60 h-screen text-slate-800 dark:text-slate-50 p-5 fixed left-0 top-0 shadow-md">
+  const pathName=usePathname()
+  const sidebarLinks =[
+    {
+      title:"Dashboard",
+      icon:LayoutGrid,
+      href:"/dashboard"
+    },
     
-      <Link className="mb-6" href="#" >
-        Logo
+    {
+      title:"Catalogue",
+      icon:Slack,
+       href:"/dashboard/Catalogue"
+    },
+    
+    {
+      title:"Customers",
+      icon:Users2,
+       href:"/dashboard/Customers"
+    },
+    {
+      title:"Orders",
+      icon:Truck,
+       href:"/dashboard/Orders"
+    },
+    {
+      title:"Markets",
+      icon:Warehouse,
+    
+       href:"/dashboard/Markets"
+    },
+    {
+      title:"Farmers",
+      icon:Tractor
+      ,
+       href:"/dashboard/Farmers"
+    },
+
+
+    {
+      title:"Staffs",
+      icon:UserSquare2
+      ,
+       href:"/dashboard/Staffs"
+    },
+
+
+    {
+      title:"Settings",
+      icon:LayoutGrid,
+      href:"/dashboard/settings"
+    },
+
+    {
+      title:"Online Store",
+      icon:ExternalLink,
+      href:"/dashboard/onlineStore"
+    },
+  ];
+  return (
+    <div className="dark:bg-slate-700 bg-slate-50 space-y-6 w-60 h-screen text-slate-800 dark:text-slate-50 fixed left-0 top-0 shadow-md">
+    
+      <Link className="" href="#" >
+        {/*<Image src={logo} alt="company logo " className="w-36" height={190} />*/}
       </Link>
 
-      <div className="space-y-4 flex flex-col">
-      <Link href="#">  Dashboard </Link>
-      <Link href="#">  Catalogue  </Link>
-      <Link href="#">  Customers </Link>
-      <Link href="#">  Markets </Link>
-      <Link href="#">  Farmers</Link>
-      <Link href="#">  Others </Link>
-      <Link href="#">  Staffs </Link>
-      <Link href="#">  Settings </Link>
-      <Link href="#">  Online Store </Link>
+      <div className="space-y-4 flex flex-col mt-14">
+      <Link href="#" className="flex items-center space-x-3 
+      px-6 py-2 border-l-4 border-green-600 "> 
+      <LayoutGrid/>
+       <span>Dashboard </span>
+       </Link>
+       <Link href="#" className="flex items-center space-x-3 
+      px-6 py-2 border-l-4 border-green-600 "> 
+      <Slack/>
+       <span>Catalogue </span>
+       </Link>
+
+       {
+        sidebarLinks.map((item,i)=>{
+          const Icon= item.icon
+
+            return (
+              <Link key={i} href={item.href} className={`flex items-center space-x-3 
+                px-6 py-2 border-l-4 border-green-600`}> 
+                <Icon/>
+                 <span>{item.title} </span>
+                 </Link>
+            );
+        })
+       }
+
       </div>
     </div>
   )
