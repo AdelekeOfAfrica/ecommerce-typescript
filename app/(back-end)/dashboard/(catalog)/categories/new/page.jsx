b8c28060fbacb9e6dc9eab1b61113d401c29359b
AdelekeOfAfrica <adelekeofafrica@gmail.com>
2025-03-19 17:27:23 +0100

    worked on linking the app with uploadthing

--- app/(back-end)/dashboard/(catalog)/categories/new/page.jsx
+++ app/(back-end)/dashboard/(catalog)/categories/new/page.jsx
@@ -1,14 +1,17 @@
 "use client"
 import { X } from 'lucide-react'
-import React from 'react'
+import React, { useState } from 'react'
 import FormHeader from '../../../../../../components/backoffice/FormHeader';
 import TextInput from '../../../../../../components/FormInputs/TextInput';
 import SubmitButton from '../../../../../../components/FormInputs/SubmitButton';
 import { useForm } from 'react-hook-form';
 import TextAreaInput from '../../../../../../components/FormInputs/TextAreaInput';
 import { generateSlug } from '../../../../../../lib/generateSlug';
+import ImageInput from '../../../../../../components/FormInputs/ImageInput'
 
 export default function NewCategory() {
+
+  const [imageUrl,setImageUrl] = useState("")
   const {register,handleSubmit,formState:{errors}}=useForm();
   async function onSubmit(data){
     const slug = generateSlug(data.title);
@@ -27,6 +30,7 @@ export default function NewCategory() {
         errors={errors}/>
         <TextAreaInput label="Category Description" name="Description" register={register}
         errors={errors} />
+        <ImageInput imageUrl={imageUrl} endpoint="categoryImageUploader" setImageUrl={setImageUrl} label="Category Image"/>
       </div>
       <SubmitButton isLoading={false} buttonTitle="category Title" loadingButtonTitle="creating category please wait ..."/>
     </form>
