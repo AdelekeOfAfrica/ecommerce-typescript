183842b0824538a0b44bc8235052859a409bde46
AdelekeOfAfrica <adelekeofafrica@gmail.com>
2025-04-14 22:13:55 +0100

    reinstalled prisma

--- app/api/banners/route.js
+++ app/api/banners/route.js
@@ -1,9 +1,16 @@
 import { NextResponse } from "next/server";
+import db from "../../../lib/db";
 
 export async function POST(request){
 try{
     const {title,slug,imageUrl,link,isActive} = await request.json();
-    const newBanner = {title,slug,imageUrl,link,isActive};
+   // const newBanner = {title,slug,imageUrl,link,isActive};
+   const newBanner = await db.banner.create({
+    data:{
+        title,slug,imageUrl,link,isActive
+    }
+   });
+
     console.log(newBanner)
     return NextResponse.json(newBanner);
 
